62fc33aac891f0beb04d60dee1f8810bfbbbd330
nargok <nargok@gmail.com>
2023-11-06 07:55:30 +0900

    use ECR module

--- envs/modules/ecr/main.tf
+++ envs/modules/ecr/main.tf
@@ -6,13 +6,18 @@ resource "aws_ecr_repository" "this" {
     }
 }
 
+# ECRは保存されているイメージの容量に応じて料金がかかる。古いイメージを自動削除するための設定
 resource "aws_ecr_lifecycle_policy" "this" {
+  # JSONの記述方法は３つある
+  # 1. ヒアドキュメント
+  # 2. ファイルから読み込むfile(), templatefile()
+  # 3. jsonencode()にjson文字列を渡す
     policy = jsonencode(
         {
             "rules": [
                 {
                     "rulePriority": 1,
-                    "description": "Hold only 10 ${var.holding_count}",
+                    "description": "Hold only ${var.holding_count} images, expire all others",
                     "selection" : {
                         "tagStatus": "any",
                         "countType": "imageCountMoreThan",
--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -1,35 +1,5 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "example-prod-foobar-nginx"
+module "nginx" {
+    source = "../../../modules/ecr"
 
-  tags = {
-    Name = "example-prod-foobar-nginx"
-  }
-}
-
-# ECRは保存されているイメージの容量に応じて料金がかかる。古いイメージを自動削除するための設定
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  # JSONの記述方法は３つある
-  # 1. ヒアドキュメント
-  # 2. ファイルから読み込むfile(), templatefile()
-  # 3. jsonencode()にjson文字列を渡す
-  policy = jsonencode(
-    {
-      "rules" : [
-        {
-          "rulePriority" : 1,
-          "description" : "Hold only 10 images, expire all others",
-          "selection" : {
-            "tagStatus" : "any",
-            "countType" : "imageCountMoreThan",
-            "countNumber" : 10
-          },
-          "action" : {
-            "type" : "expire"
-          }
-        }
-      ]
-    }
-  )
-
-  repository = aws_ecr_repository.nginx.name
+    name = "example-prod-foobar-nginx"
 }
