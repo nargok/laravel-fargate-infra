6f92032de64b141dc725efd0ba1041e2690e59bd
nargok <nargok@gmail.com>
2023-11-05 11:07:27 +0900

    Attach ECR policy

--- envs/prod/app/foobar/ecr.tf
+++ envs/prod/app/foobar/ecr.tf
@@ -5,3 +5,31 @@ resource "aws_ecr_repository" "nginx" {
       Name = "example-prod-foobar-nginx"
     }
 }
+
+# ECRは保存されているイメージの容量に応じて料金がかかる。古いイメージを自動削除するための設定
+resource "aws_ecr_lifecycle_policy" "nginx" {
+    # JSONの記述方法は３つある
+    # 1. ヒアドキュメント
+    # 2. ファイルから読み込むfile(), templatefile()
+    # 3. jsonencode()にjson文字列を渡す
+    policy = jsonencode(
+        {
+            "rules": [
+                {
+                    "rulePriority": 1,
+                    "description": "Hold only 10 images, expire all others",
+                    "selection" : {
+                        "tagStatus": "any",
+                        "countType": "imageCountMoreThan",
+                        "countNumber": 10
+                    },
+                    "action": {
+                        "type": "expire"
+                    }
+                }
+            ]
+        }
+    )
+
+    repository = aws_ecr_repository.nginx.name
+}
