26cb274951382321b66e81e22bc3e91d53d8b431
nargok <nargok@gmail.com>
2023-11-07 20:21:40 +0900

    use local variables

--- envs/modules/ecr/variable.tf
+++ envs/modules/ecr/variable.tf
@@ -1,3 +1,9 @@
+/**
+* terraform実行時に値を設定できる
+* 1. コマンドライン引数で設定 terraform apply -var="name=foobar"
+* 2. tfvarsファイルで設定 terraform apply -var-file="foobar.tfvars"
+* 3. moduleの呼び出し元で設定
+*/
 variable "name" {
     type = string
 }
---
+++ envs/prod/app/foobar/local.tf
@@ -0,0 +1,7 @@
+/**
+* Local Valursは外からの代入はできない
+* 繰り返し利用する値を1箇所で定義するために使う
+*/
+locals {
+    service_name = "foobar"
+}
